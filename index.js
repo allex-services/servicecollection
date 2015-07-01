13dd2d731435d11dcd75a69af91524e478e7d7c1
Andrija Petrovic <andrija.hers@gmail.com>
2015-07-01 11:23:50 +0200

    execSuite.registry.register just became asynchronous

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function createServicePack(execlib){
   'use strict';
-  var execSuite = execlib.execSuite,
-      servicecontainerServicePack = execSuite.registry.register('allex_servicecontainerservice'),
-      ParentServicePack = servicecontainerServicePack;
+  var lib = execlib.lib,
+    q = lib.q,
+    d = q.defer(),
+    execSuite = execlib.execSuite;
 
-  return {
-    Service: require('./servicecreator')(execlib,ParentServicePack),
-    SinkMap: require('./sinkmapcreator')(execlib,ParentServicePack)
-  };
+  execSuite.registry.register('allex_servicecontainerservice').done(
+    realCreator.bind(null, d),
+    d.reject.bind(d)
+  );
+
+  function realCreator(defer, ParentServicePack) {
+    defer.resolve({
+      Service: require('./servicecreator')(execlib,ParentServicePack),
+      SinkMap: require('./sinkmapcreator')(execlib,ParentServicePack)
+    });
+  }
+
+  return d.promise;
 }
 
 module.exports = createServicePack;
